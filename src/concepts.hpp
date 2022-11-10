ac9a179c0f1b5b91af826ceb89e5a562bd45a490
Carmine Margiotta <cmargiotta@posteo.net>
2022-11-10 08:40:16 +0100

    Deleted old files

--- src/concepts.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef CONCEPTS_HPP
-#define CONCEPTS_HPP
-
-#include <concepts/iterator.hpp>
-#include <concepts/member_exists.hpp>
-
-#endif// CONCEPTS_HPP
--- src/utils.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef PARSER_UTILS_HPP
-#define PARSER_UTILS_HPP
-
-#include "utils/fixed_endianness.hpp"
-
-#endif// PARSER_UTILS_HPP
